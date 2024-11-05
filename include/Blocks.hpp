c6c407df5a01376b1b9d04038535d62c26364bcd
snirGH <snir.bender@gmail.com>
2024-11-05 11:51:44 -0500

    Added Blocks Class

---
+++ include/Blocks.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+constexpr uint8_t BLOCK_ROWS = 3;
+constexpr uint8_t BLOCK_COLS = 4;
+constexpr float BLOCK_POS = 50.0f;
+constexpr float BLOCK_WIDTH = 100.0f;
+constexpr float BLOCK_HEIGHT = 40.0f;
+constexpr float BLOCK_OFFSET = 30.0f;
+
+class Blocks {
+ public:
+  Blocks() {
+    blocks.resize(BLOCK_ROWS);
+    for (auto& row : blocks)
+      row.resize(BLOCK_COLS);
+    GenerateBlocks();
+  };
+
+  void Draw(sf::RenderTarget& rt) {
+    for (std::vector<sf::RectangleShape>& blockVec : blocks) {
+      for (sf::RectangleShape& block : blockVec) {
+        rt.draw(block);
+      }
+    }
+  }
+
+  void RemoveBlock(size_t row, size_t col) {
+    blocks[row].erase(blocks[row].begin() + col);
+  }
+
+  void GenerateBlocks() {
+    sf::Vector2f pos = {BLOCK_POS, BLOCK_POS};
+    for (size_t i = 0; i < BLOCK_ROWS; i++) {
+      for (size_t j = 0; j < BLOCK_COLS; j++) {
+        sf::RectangleShape rect({BLOCK_WIDTH, BLOCK_HEIGHT});
+        rect.setPosition(pos);
+        blocks[i][j] = rect;
+        pos.x += BLOCK_WIDTH + BLOCK_OFFSET;
+      }
+      pos.y += BLOCK_HEIGHT + BLOCK_OFFSET;
+      pos.x = BLOCK_POS;
+    }
+  }
+
+ private:
+  std::vector<std::vector<sf::RectangleShape>> blocks;
+};
--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,17 @@
 #include <Ball.hpp>
+#include <Blocks.hpp>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(600, 900), "Breakout");
   sf::CircleShape circle;
   sf::Vector2f windowSize = static_cast<sf::Vector2f>(window.getSize());
 
+  Blocks blocks;
+
   Player player({windowSize.x / 2, windowSize.y - 50.0f});
-  Ball ball({50.0f, 50.0f});
+  Ball ball({300.0f, 300.0f});
   ball.SetDirection({1.0f, 1.0f});
+  blocks.RemoveBlock(1, 1);
 
   sf::Clock clock;
 
@@ -26,6 +30,7 @@ int main() {
     player.Update(window);
 
     window.clear();
+    blocks.Draw(window);
     player.Draw(window);
     ball.Draw(window);
     window.display();
